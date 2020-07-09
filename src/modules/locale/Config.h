448c40031ef033e8d58f3c42f67c0d23ecc274b9
Adriaan de Groot <groot@kde.org>
2020-07-09 14:02:09 +0200

    [locale] Reboot the Config object
    This totally breaks the (non-functional) localeq module, though.
    That will get patched up eventually.

--- src/modules/locale/Config.h
+++ src/modules/locale/Config.h
@@ -1,7 +1,7 @@
 /* === This file is part of Calamares - <https://github.com/calamares> ===
  *
- *   Copyright 2019-2020, Adriaan de Groot <groot@kde.org>
- *   Copyright 2020, Camilo Higuita <milo.h@aol.com>
+ *   SPDX-FileCopyrightText: 2020 Adriaan de Groot <groot@kde.org>
+ *   SPDX-License-Identifier: GPL-3.0-or-later
  *
  *   Calamares is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -20,77 +20,17 @@
 #ifndef LOCALE_CONFIG_H
 #define LOCALE_CONFIG_H
 
-#include "LocaleConfiguration.h"
-
-#include "Job.h"
-#include "locale/TimeZone.h"
-
-#include <QAbstractListModel>
 #include <QObject>
-
-#include <memory>
 
 class Config : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY( CalamaresUtils::Locale::CStringListModel* zonesModel READ zonesModel CONSTANT FINAL )
-    Q_PROPERTY( CalamaresUtils::Locale::CStringListModel* regionModel READ regionModel CONSTANT FINAL )
-    Q_PROPERTY( QString prettyStatus READ prettyStatus NOTIFY prettyStatusChanged FINAL )
 
 public:
     Config( QObject* parent = nullptr );
-    ~Config();
+    ~Config() override;
 
     void setConfigurationMap( const QVariantMap& );
-
-    void setLocaleInfo( const QString& initialRegion, const QString& initialZone, const QString& localeGenPath );
-
-    Calamares::JobList createJobs();
-    QMap< QString, QString > localesMap();
-
-public Q_SLOTS:
-    CalamaresUtils::Locale::CStringListModel* regionModel() const;
-    CalamaresUtils::Locale::CStringListModel* zonesModel() const;
-
-    /** @brief The status for the summary before the exec: step
-     *
-     * This is a multi-line description of what the locale module
-     * will do; @see ViewStep::prettyStatus().
-     */
-    QString prettyStatus() const;
-
-signals:
-    void prettyStatusChanged();
-
-private:
-    /// @brief based on available locales, guess a good default
-    LocaleConfiguration guessLocaleConfiguration() const;
-
-    // For the given locale config, return two strings describing
-    // the settings for language and numbers.
-    std::pair< QString, QString > prettyLocaleStatus( const LocaleConfiguration& ) const;
-
-    /** @brief Update the GS *locale* key with the selected system language.
-     *
-     * This uses whatever is set in m_selectedLocaleConfiguration as the language,
-     * and writes it to GS *locale* key (as a string, in BCP47 format).
-     */
-    void updateGlobalLocale();
-    void updateGlobalStorage();
-    void updateLocaleLabels();
-
-    const CalamaresUtils::Locale::TZZone* currentLocation() const;
-
-    CalamaresUtils::Locale::CStringPairList m_regionList;
-    CalamaresUtils::Locale::CStringListModel* m_regionModel;
-    CalamaresUtils::Locale::CStringListModel* m_zonesModel;
-
-    LocaleConfiguration m_selectedLocaleConfiguration;
-
-    QString m_localeGenPath;
-    QStringList m_localeGenLines;
-
-    int m_currentRegion = -1;
 };
 
 
